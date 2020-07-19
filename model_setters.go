a568707892ab8467b92b169b63f335b395c3a10e
Tudor Hulban <tudi@J3710>
2020-07-19 13:39:06 +0300

    added recon test

--- model_setters.go
+++ model_setters.go
@@ -4,7 +4,7 @@ import (
 	"context"
 )
 
-// Set sets or updates key in store.
+// SetKV sets or updates key in store.
 func (s ETCDStore) SetKV(ctx context.Context, theKV KV) error {
 	_, errSet := s.TheStore.Put(ctx, theKV.key, theKV.value)
 	return errSet
--- t_setters_test.go
+++ t_setters_test.go
@@ -13,12 +13,17 @@ const testKey = "the key"
 const testValue1 = "the value1"
 const testValue2 = "the value2"
 
-// tests insert and update of key
+// Target of test:
+// a. insert
+// b. update
+// c. read
+// d. read after reconnect
+// e. close
 func TestSet(t *testing.T) {
-	l, errLog := loginfo.New(2)
-	assert.Nil(t, errLog)
+	l1, errLog1 := loginfo.New(2)
+	assert.Nil(t, errLog1)
 
-	storeClient, err := NewETCDClient([]string{etcdURL}, l)
+	storeClient, err := NewETCDClient([]string{etcdURL}, l1)
 	assert.Nil(t, err)
 	assert.NotNil(t, storeClient)
 
@@ -26,18 +31,18 @@ func TestSet(t *testing.T) {
 		assert.Nil(t, storeClient.TheStore.Close())
 	}()
 
-	// test set
+	// test set - a.
 	assert.Nil(t, storeClient.SetKV(context.Background(), KV{
 		key:   testKey,
 		value: testValue1,
 	}))
 
-	// test read key
+	// test read key - c.
 	val1, errGet1 := storeClient.GetVByK(context.Background(), testKey)
 	assert.Nil(t, errGet1)
 	assert.Equal(t, testValue1, val1)
 
-	// test update
+	// test update - b.
 	assert.Nil(t, storeClient.SetKV(context.Background(), KV{
 		key:   testKey,
 		value: testValue2,
@@ -47,4 +52,21 @@ func TestSet(t *testing.T) {
 	val2, errGet2 := storeClient.GetVByK(context.Background(), testKey)
 	assert.Nil(t, errGet2)
 	assert.Equal(t, testValue2, val2)
+
+	// close connection
+	assert.Nil(t, storeClient.Close()) // e.
+
+	l2, errLog2 := loginfo.New(2)
+	assert.Nil(t, errLog2)
+
+	// reconnect in order to recheck key
+	reconClient, errRecon := NewETCDClient([]string{etcdURL}, l2)
+	assert.Nil(t, errRecon)
+	assert.NotNil(t, reconClient)
+	defer reconClient.Close()
+
+	// test read key - d.
+	val3, errGet3 := reconClient.GetVByK(context.Background(), testKey)
+	assert.Nil(t, errGet3)
+	assert.Equal(t, testValue2, val3)
 }
